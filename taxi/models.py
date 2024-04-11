347fca76e72269c16a0b8635e091fe6f6fb7ca6a
Artom <artem.umantsev@gmail.com>
2024-04-11 20:31:01 +0300

    'Fixed'

---
+++ taxi/models.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"(name: {self.name}, country: {self.country})"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacture = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Driver"
+
+    def __str__(self):
+        return f"{self.username}"
